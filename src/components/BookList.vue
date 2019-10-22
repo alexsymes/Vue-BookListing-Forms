594c7943a633e7801cfc4ba6210abd1f738a64e6
AlexSymes <asymes@nanthealth.com>
2019-10-22 15:22:04 +0100

    Part4

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
     <h1>{{title}}</h1>
+    <input v-model="searchInput" type="text" placeholder="Search Books">
     <ul>
-      <book-item v-for='book in books' :key='book.id' :book='book'></book-item>
+      <book-item v-for='book in searchedBooks' :key='book.id' :book='book'></book-item>
     </ul>
     <hr>
     <h2>Filtered Books By Ownership</h2>
@@ -24,6 +25,7 @@ export default {
   name: "BookList",
   data() {
     return {
+      searchInput: "",
       filters: ["bought", "borrowed"],
       holding: "bought",
       title: "All Books",
@@ -42,6 +44,12 @@ export default {
   computed: {
     filteredBooks() {
       return _.filter(this.books, ["ownership", this.holding]);
+    },
+    searchedBooks(){
+      const searchFilter = book => {
+        return book.title.toLowerCase().match(this.searchInput.toLowerCase());
+      };
+      return _.filter(this.books, searchFilter);
     }
   },
   methods: {
